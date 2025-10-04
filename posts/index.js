aea3f926b563fd10043d66cca1c26be427e085b8
Junichi Kako <cjaucono@gmail.com>
2025-10-04 11:05:30 +0900

    feat:投稿関連のAPIの作成

---
+++ posts/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+app.use(express.json());
+
+const post = {};
+
+app.get('/posts', (req, res) => {
+  res.send(post);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  post[id] = { id, title };
+  res.status(201).send(post[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
